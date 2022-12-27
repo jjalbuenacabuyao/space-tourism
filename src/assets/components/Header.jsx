c8d9ed08a8931b1027da3f9f814f9886744721de
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-27 20:50:48 +0800

    use the nav toggle component

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from './Logo';
 import Nav from './Nav';
+import NavToggle from './NavToggle';
 import data from '../constants/data.json';
 
 const Header = () => {
@@ -8,6 +9,7 @@ const Header = () => {
   return (
     <header className='pl-6 py-6 flex'>
       <Logo src={ logo } />
+      <NavToggle />
       <Nav />
     </header>
   )

bb2aa73526feb9282dfa32a68386770d9f0143e6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-27 20:35:10 +0800

    created the markup for nav

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import data from '../constants/data.json';
 
 const Nav = () => {
+  const links = data.home.links;
+  const [home, destination, crew, tech] = links;
   return (
-    <div>Nav</div>
+    <nav>
+      <ul>
+        <li><a href="#">{ home }</a></li>
+        <li><a href="#">{ destination }</a></li>
+        <li><a href="#">{ crew }</a></li>
+        <li><a href="#">{ tech }</a></li>
+      </ul>
+    </nav>
   )
 }
 

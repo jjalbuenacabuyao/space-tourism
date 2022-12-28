c3dd81d4955483ef4540e7276cf13f3cb1441dee
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-28 15:31:08 +0800

    set z-index

--- src/assets/components/NavToggle.jsx
+++ src/assets/components/NavToggle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavToggle = props => {
   return (
-    <button className={`h-[21px] w-6 ${ props.isOpened ? "bg-close" : "bg-hamburger"} bg-no-repeat`} onClick={ props.toggle }>
+    <button className={`h-[21px] w-6 ${ props.isOpened ? "bg-close" : "bg-hamburger"} bg-no-repeat z-50`} onClick={ props.toggle }>
       <span className='sr-only'>Toggle Menu</span>
     </button>
   )

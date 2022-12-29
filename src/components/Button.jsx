5fea6453494784a04574aefa242a418f134cc6cb
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 17:04:10 +0800

    link to destination page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import data from '../constants/data.json';
+import React from "react";
+import data from "../constants/data.json";
+import { Link } from "react-router-dom";
 
 const Button = () => {
   const home = data.home;
   return (
-    <button className='text-xl tracking-[1.25px] uppercase px-30px py-68px font-bellefair text-clr-dark bg-white rounded-[50%]'>
-      { home.btnText }
-    </button>
-  )
-}
+    <Link to="/destination">
+      <button className="text-xl tracking-[1.25px] uppercase px-30px py-68px font-bellefair text-clr-dark bg-white rounded-[50%]">
+        {home.btnText}
+      </button>
+    </Link>
+  );
+};
 
-export default Button
+export default Button;

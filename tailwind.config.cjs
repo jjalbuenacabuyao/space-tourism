e60f1e190d4ab0f50a3fdf6d43e22e9713459fe1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 16:58:01 +0800

    updated config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "clr-dark": "hsl(230, 35, 7%)",
+        "clr-dark": "#0B0D17",
         "off-white": "#D0D6F9",
         "semi-transparent": "hsla(0, 0, 100%, 0.04)"
       },
@@ -30,8 +30,10 @@ module.exports = {
       },
       padding: {
         "responsive": "clamp(1.5rem, 0.82rem + 2.91vw, 3.4375rem)",
-        "home-hero": "clamp(1.5rem, -6.61rem + 34.61vw, 10rem)"
-      }
+        "home-hero": "clamp(1.5rem, -6.61rem + 34.61vw, 10rem)",
+        "30px": "1.875rem",
+        "68px": "4.25rem"
+      },
     },
   },
   plugins: [],

2260ff168d0b9cb6fe568a947e99cc66c918c9d3
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 16:59:11 +0800

    used flex

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroText from "./HeroText";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <main className="bg-home-mobile min-h-screen grid place-items-center px-home-hero bg-no-repeat bg-cover bg-center bg-fixed">
+    <main className="bg-home-mobile min-h-screen flex flex-col items-center justify-center px-home-hero bg-no-repeat bg-cover bg-center bg-fixed gap-20 pt-28 pb-12">
       <HeroText />
+      <Button />
     </main>
   );
 };

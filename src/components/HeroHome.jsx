8e4e447ac586fbd10177fc0397a41ab3e64b44f4
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 17:30:30 +0800

    renamed the classname

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const HeroHome = () => {
   return (
-    <main className="bg-home-mobile min-h-screen flex flex-col items-center justify-center px-home-hero bg-no-repeat bg-cover bg-center bg-fixed gap-20 pt-28 pb-12">
+    <main className="bg-home-mobile min-h-screen flex flex-col items-center justify-center px-hero bg-no-repeat bg-cover bg-center bg-fixed gap-20 pt-28 pb-12">
       <HeroText />
       <Button />
     </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,7 +30,7 @@ module.exports = {
       },
       padding: {
         "responsive": "clamp(1.5rem, 0.82rem + 2.91vw, 3.4375rem)",
-        "home-hero": "clamp(1.5rem, -6.61rem + 34.61vw, 10rem)",
+        "hero": "clamp(1.5rem, -6.61rem + 34.61vw, 10rem)",
         "30px": "1.875rem",
         "68px": "4.25rem"
       },

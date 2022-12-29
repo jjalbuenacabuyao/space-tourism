5b37c3ff28f3db27497ff92a314034a8d0a0e4da
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 16:58:53 +0800

    styled the component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import data from '../constants/data.json';
 
 const Button = () => {
+  const home = data.home;
   return (
-    <div>Button</div>
+    <button className='text-xl tracking-[1.25px] uppercase px-30px py-68px font-bellefair text-clr-dark bg-white rounded-[50%]'>
+      { home.btnText }
+    </button>
   )
 }
 

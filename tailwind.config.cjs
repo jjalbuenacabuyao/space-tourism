520e52c40c7cfcacb9384b4af2173f7a40ae99e5
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 10:58:32 +0800

    added bg-images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,7 +21,10 @@ module.exports = {
       },
       backgroundImage: {
         "close": "url('./assets/images/shared/icon-close.svg')",
-        "hamburger": "url('./assets/images/shared/icon-hamburger.svg')"
+        "hamburger": "url('./assets/images/shared/icon-hamburger.svg')",
+        "home-mobile": "url('./assets/images/home/background-home-mobile.jpg')",
+        "home-tablet": "url('./assets/images/home/background-home-tablet.jpg')",
+        "home-desktop": "url('./assets/images/home/background-home-desktop.jpg')"
       },
       padding: {
         "responsive": "clamp(1.5rem, 0.82rem + 2.91vw, 3.4375rem);"

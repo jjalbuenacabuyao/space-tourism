099081bf2af14ad135d14c6ebee543768e5745b8
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 10:18:18 +0800

    export components in a single file

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Button from './Button';
+import Header from './Header';
+import Hero from './Hero';
+import HeroText from './HeroText';
+import Logo from './Logo';
+import Nav from './Nav';
+import NavToggle from './NavToggle';
+
+export {
+  Button,
+  Header,
+  Hero,
+  HeroText,
+  Logo,
+  Nav,
+  NavToggle
+}

f5220841ac7e916f7a8820a7198fb5e35c927b05
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 11:47:49 +0800

    styled the component

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -4,10 +4,11 @@ import data from '../constants/data.json';
 const HeroText = () => {
   const home = data.home;
   return (
-    <article>
-      <h1 className='uppercase font-barlow-condensed text-base'>
-        { home.heading }<span className='font-bellefair'>{ home.highlight }</span>
+    <article className='text-center'>
+      <h1 className='uppercase font-barlow-condensed text-base text-heading leading-[3em] tracking-[2.7px]'>
+        { home.heading }<br /><span className='font-bellefair text-highlight tracking-0 leading-[1.3em]'>{ home.highlight }</span>
       </h1>
+      
       <p>{ home.description }</p>
     </article>
   )

540537fc47b6e0728c2821c0e04019d41a6cd262
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 11:22:32 +0800

    styled the container

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import HeroText from './HeroText';
+import React from "react";
+import HeroText from "./HeroText";
 
 const Hero = () => {
   return (
-    <main>
+    <main className="bg-home-mobile min-h-screen grid place-items-center px-home-hero bg-no-repeat bg-cover bg-center bg-fixed">
       <HeroText />
     </main>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

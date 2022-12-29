d4a182c4cc46f571584b24b2558531515faedec1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 10:20:15 +0800

    rendered home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './containers/Home';
 
 const App = () => {
   return (
     <>
-      <Header />
+      <Home />
     </>
   )
 }

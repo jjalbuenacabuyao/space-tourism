f6fcfe6d9ae844b540ba20f89b9c8d61c450b349
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 10:12:46 +0800

    fixed the hsl

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,14 +7,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "clr-dark": "hsl(230°, 35%, 7%)",
-        "off-white": "hsl(231°, 77%, 90%)",
+        "clr-dark": "hsl(230, 35, 7%)",
+        "off-white": "hsl(231, 77, 90%)",
         "semi-transparent": "hsla(0, 0, 100%, 0.04)"
       },
       fontFamily: {
         "barlow": ["Barlow", "sans-serif"],
         "barlow-condensed": ["Barlow Condensed", "sans-serif"],
         "bellefair": ["Bellefair", "serif"]
+      },
+      fontSize: {
+        "fs-15": "0.9375rem",
       },
       backgroundImage: {
         "close": "url('./assets/images/shared/icon-close.svg')",

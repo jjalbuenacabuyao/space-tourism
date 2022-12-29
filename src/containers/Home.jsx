b0a1bf1c58ac9225ec8119c511b6289e79c2ee20
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-29 10:54:36 +0800

    rendered hero

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Header } from '../components';
+import { Header, Hero } from '../components';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }

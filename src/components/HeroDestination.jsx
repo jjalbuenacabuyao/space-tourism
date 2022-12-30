f428676be8f5c3fbda1bac1b4b8d216ddf5c4d4b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-30 18:46:51 +0800

    used the numbered heading

--- src/components/HeroDestination.jsx
+++ src/components/HeroDestination.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import HeroContainer from './HeroContainer';
-import data from '../constants/data.json';
-import TabMenu from './TabMenu';
+import React from "react";
+import HeroContainer from "./HeroContainer";
+import data from "../constants/data.json";
+import TabMenu from "./TabMenu";
+import NumberedHeading from "./NumberedHeading";
 
 const HeroDestination = () => {
   const heading = data.destinations[0].heading;
   const num = data.destinations[0].num;
   return (
     <HeroContainer px="px-hero" pb="pb-[3.75rem]" bg="bg-dest-mobile">
-      <h1 className='text-center uppercase text-base tracking-[2.7px] font-barlow-condensed'><span className='font-bold mr-4' aria-hidden="true">{ num }</span>{ heading }</h1>
+      <NumberedHeading num={num} text={text} />
       <TabMenu />
     </HeroContainer>
-  )
-}
+  );
+};
 
-export default HeroDestination
+export default HeroDestination;

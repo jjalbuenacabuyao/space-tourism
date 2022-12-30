05f172f5ec86e0cbaed5166b0d719fb181b7c53c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-30 20:04:46 +0800

    remove li element

--- src/components/Tablist.jsx
+++ src/components/Tablist.jsx
@@ -4,13 +4,11 @@ const Tablist = props => {
   const buttonStyle = "font-barlow-condensed uppercase tracking-[2.36px] pb-3";
   const isActive = props.active == props.planet;
   return (
-    <li>
-      <button 
+    <button 
       onClick={() => props.setter(props.planet)} 
       className={`${buttonStyle} ${isActive ? "border-b-[3px] border-b-off-white" : ""}`} 
       aria-selected={isActive ? "true" : "false"}
-      >{props.planet.name}</button>
-    </li>
+    >{props.planet.name}</button>
   )
 }
 

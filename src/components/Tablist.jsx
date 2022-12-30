2ca47b6375955851f3b6ef0af981f0f0649aa901
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-30 19:45:03 +0800

    added aria-selected

--- src/components/Tablist.jsx
+++ src/components/Tablist.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Tablist = props => {
   const buttonStyle = "font-barlow-condensed uppercase tracking-[2.36px] pb-3";
+  const isActive = props.active == props.planet;
   return (
     <li>
       <button 
-      onClick={() => props.setter(props.planet)} className={`${buttonStyle} ${props.active == props.planet ? "border-b-[3px] border-b-off-white" : ""}`} 
+      onClick={() => props.setter(props.planet)} 
+      className={`${buttonStyle} ${isActive ? "border-b-[3px] border-b-off-white" : ""}`} 
+      aria-selected={isActive ? "true" : "false"}
       >{props.planet.name}</button>
     </li>
   )

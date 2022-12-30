a076df597cbd62531c6e34fde5a3cac8c6d8fdc4
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-30 19:36:41 +0800

    use TabList component

--- src/components/TabMenu.jsx
+++ src/components/TabMenu.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import data from "../constants/data.json";
+import Tablist from "./Tablist";
 
 const TabMenu = () => {
   const destinations = data.destinations;
   //First item is not included
   const [, moon, mars, europa, titan] = destinations;
   const [active, setActive] = useState(moon);
-  const buttonStyle = "font-barlow-condensed uppercase tracking-[2.36px] pb-3";
+  const planets = [moon, mars, europa, titan];
+  let listItems = planets.map(planet => <Tablist planet={planet} active={active} setter={setActive} />);
 
   return (
     <div className="text-center flex flex-col items-center gap-8">
       <picture>
-        <source srcset={active.images.webp} />
-        <source srcset={active.images.png} />
+        <source srcSet={active.images.webp} />
+        <source srcSet={active.images.png} />
         <img
           src={active.images.png}
           alt={active.imgAlt}
@@ -20,64 +22,31 @@ const TabMenu = () => {
         />
       </picture>
       <ul className="flex gap-6 items-center text-sm">
-        <li>
-          <button
-            onClick={() => setActive(moon)}
-            aria-selected={active == moon ? "true" : "false"}
-            className={`${buttonStyle} ${
-              active == moon ? "border-b-[3px] border-b-off-white" : ""
-            }`}
-          >
-            {moon.name}
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setActive(mars)}
-            aria-selected={active == mars ? "true" : "false"}
-            className={`${buttonStyle} ${
-              active == mars ? "border-b-[3px] border-b-off-white" : ""
-            }`}
-          >
-            {mars.name}
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setActive(europa)}
-            aria-selected={active == europa ? "true" : "false"}
-            className={`${buttonStyle} ${
-              active == europa ? "border-b-[3px] border-b-off-white" : ""
-            }`}
-          >
-            {europa.name}
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setActive(titan)}
-            aria-selected={active == titan ? "true" : "false"}
-            className={`${buttonStyle} ${
-              active == titan ? "border-b-[3px] border-b-off-white" : ""
-            }`}
-          >
-            {titan.name}
-          </button>
-        </li>
+        {listItems}
       </ul>
       <article>
         <div className="flex flex-col gap-6 pb-8 border-b-[1px] border-b-off-white">
-          <h1 className="text-planet-name uppercase font-bellefair">{active.name}</h1>
+          <h1 className="text-planet-name uppercase font-bellefair">
+            {active.name}
+          </h1>
           <p>{active.description}</p>
         </div>
         <div className="flex flex-col gap-8 pt-8">
           <div className="flex flex-col gap-3">
-            <p className="uppercase text-sm font-barlow-condensed">{active.distanceTitle}</p>
-            <p className="uppercase font-bellefair text-[1.75rem]">{active.distance}</p>
+            <p className="uppercase text-sm font-barlow-condensed">
+              {active.distanceTitle}
+            </p>
+            <p className="uppercase font-bellefair text-[1.75rem]">
+              {active.distance}
+            </p>
           </div>
           <div className="flex flex-col gap-3">
-            <p className="uppercase text-sm font-barlow-condensed">{active.travelTitle}</p>
-            <p className="uppercase font-bellefair text-[1.75rem]">{active.travel}</p>
+            <p className="uppercase text-sm font-barlow-condensed">
+              {active.travelTitle}
+            </p>
+            <p className="uppercase font-bellefair text-[1.75rem]">
+              {active.travel}
+            </p>
           </div>
         </div>
       </article>

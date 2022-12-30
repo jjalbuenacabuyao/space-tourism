a4ecb969794b9ddbb416d7b8a5b2d61ee4dc0dcc
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-30 10:58:56 +0800

    added styles

--- src/components/HeroContainer.jsx
+++ src/components/HeroContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroContainer = props => {
   return (
-    <main className={`${props.px} ${props.pb} min-h-screen pt-20 flex flex-col`}>
+    <main className={`${props.px} ${props.pb} ${props.bg} bg-center bg-cover bg-no-repeat bg-fixed min-h-screen pt-20 flex flex-col`}>
       { props.children }
     </main>
   )

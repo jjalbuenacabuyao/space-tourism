7214f84f7e82854adffbb17f4d562263163f1ee0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-30 18:48:50 +0800

    import and export components

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,10 @@ import HeroText from './HeroText';
 import Logo from './Logo';
 import Nav from './Nav';
 import NavToggle from './NavToggle';
+import HeroDestination from './HeroDestination';
+import NumberedHeading from './NumberedHeading';
+import HeroContainer from './HeroContainer';
+import TabMenu from './TabMenu';
 
 export {
   Button,
@@ -13,5 +17,9 @@ export {
   HeroText,
   Logo,
   Nav,
-  NavToggle
+  NavToggle,
+  HeroDestination,
+  NumberedHeading,
+  HeroContainer,
+  TabMenu
 }

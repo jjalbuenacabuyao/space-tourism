3a33e83f9c13a6a6770ef100b46be2c84d216a87
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-31 11:24:16 +0800

    added classnames

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,15 +8,16 @@ const notVisible = "translate-x-[200%]";
 const Nav = (props) => {
   const links = data.home.links;
   const [home, destination, crew, tech] = links;
-  const liStyle = "text-base font-barlow-condensed tracking-[2.7px] uppercase";
+  const liStyle = "text-base font-barlow-condensed pl-8 tracking-[2.7px] uppercase md:py-10 md:pl-0";
   const numStyle = "font-bold mr-3";
+
   return (
     <nav
       className={`fixed top-0 bottom-0 right-0 w-[67.73%] bg-white/[.04] backdrop-blur-[81.55px] ${
         props.visible ? isVisible : notVisible
       } md:static md:w-auto md:transform-none`}
     >
-      <ul className="flex flex-col gap-9 pt-32 pl-8 md:flex-row md:py-10 md:px-12">
+      <ul className="flex flex-col gap-9 pt-32 md:flex-row md:py-0 md:px-12">
         <li className={liStyle}>
           <span className={numStyle}>00</span>
           <Link to="/">{home}</Link>

6feafeefb58a8fba2977eee1e1bd51c942af4254
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-31 10:49:34 +0800

    styled nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,23 +8,29 @@ const notVisible = "translate-x-[200%]";
 const Nav = (props) => {
   const links = data.home.links;
   const [home, destination, crew, tech] = links;
+  const liStyle = "text-base font-barlow-condensed tracking-[2.7px] uppercase";
+  const numStyle = "font-bold mr-3";
   return (
     <nav
-      className={`fixed top-0 bottom-0 right-0 w-[67.73%] ${
+      className={`fixed top-0 bottom-0 right-0 w-[67.73%] bg-white/[.04] backdrop-blur-[81.55px] ${
         props.visible ? isVisible : notVisible
-      }`}
+      } md:static md:w-auto md:transform-none`}
     >
-      <ul>
-        <li>
+      <ul className="flex flex-col gap-9 pt-32 pl-8 md:flex-row md:py-10 md:px-12">
+        <li className={liStyle}>
+          <span className={numStyle}>00</span>
           <Link to="/">{home}</Link>
         </li>
-        <li>
+        <li className={liStyle}>
+          <span className={numStyle}>01</span>
           <Link to="/destination">{destination}</Link>
         </li>
-        <li>
+        <li className={liStyle}>
+          <span className={numStyle}>02</span>
           <Link to="/crew">{crew}</Link>
         </li>
-        <li>
+        <li className={liStyle}>
+          <span className={numStyle}>03</span>
           <Link to="/technology">{tech}</Link>
         </li>
       </ul>

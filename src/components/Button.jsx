06b6889021e6c52e63bc7281bae051d9dd4b6fed
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-03 19:29:50 +0800

    adjust styles on different devices

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = () => {
   const home = data.home;
   return (
     <Link to="/destination">
-      <button className="rounded-[50%] bg-white px-30px py-68px font-bellefair text-xl uppercase tracking-[1.25px] text-clr-dark">
+      <button className="rounded-[50%] bg-white px-btn-px-m py-btn-py-m font-bellefair text-xl uppercase tracking-[1.25px] text-clr-dark md:text-[2rem] md:btn-px-md md:btn-py-md">
         {home.btnText}
       </button>
     </Link>
--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const HeroHome = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-20 bg-home-mobile bg-cover bg-fixed bg-center bg-no-repeat px-hero pt-28 pb-12">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 bg-home-mobile bg-cover bg-fixed bg-center bg-no-repeat px-hero pt-28 pb-hero-home-pb md:bg-home-tablet lg:flex-row lg:items-end lg:justify-between lg:bg-home-desktop">
       <HeroText />
       <Button />
     </main>

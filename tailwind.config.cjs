62a02772f14f62e6011a886daea723109f38b7a8
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-03 19:29:17 +0800

    added custom padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -39,8 +39,11 @@ module.exports = {
       padding: {
         "responsive": "clamp(1.5rem, 0.82rem + 2.91vw, 3.4375rem)",
         "hero": "clamp(1.5rem, -6.61rem + 34.61vw, 10rem)",
-        "30px": "1.875rem",
-        "68px": "4.25rem"
+        "btn-py-m": "clamp(4.25rem, 3.48rem + 3.29vw, 6.4375rem)",
+        "btn-px-m": "clamp(1.875rem, 1.52rem + 1.5vw, 2.875rem)",
+        "btn-px-md": "clamp(2.875rem, 2.52rem + 1.5vw, 3.875rem)",
+        "btn-py-md": "clamp(6.4375rem, 6.08rem + 1.5vw, 7.4375rem)",
+        "hero-home-pb": "clamp(3rem, 1.2rem + 7.7vw, 8.125rem)",
       },
       width: {
         "img-responsive" : "clamp(10.625rem, 4.57rem + 25.82vw, 27.8125rem)",

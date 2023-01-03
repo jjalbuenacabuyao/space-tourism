3f403ec5129a0a187baecfbd9e5555d1d8d10268
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-03 19:34:43 +0800

    change bg-image in tablet and desktop

--- src/components/HeroDestination.jsx
+++ src/components/HeroDestination.jsx
@@ -8,7 +8,7 @@ const HeroDestination = () => {
   const heading = data.destinations[0].heading;
   const num = data.destinations[0].num;
   return (
-    <HeroContainer px="px-hero" pb="pb-[3.75rem]" bg="bg-dest-mobile">
+    <HeroContainer px="px-hero" pb="pb-[3.75rem]" bg="bg-dest-mobile md:bg-dest-tablet lg:bg-dest-desktop">
       <NumberedHeading num={num} text={heading} />
       <TabMenu />
     </HeroContainer>

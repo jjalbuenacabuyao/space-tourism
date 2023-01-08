e7255e9bc49fd6d39142d8780d3fe91f160c3fba
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-08 20:59:51 +0800

    adjusted padding

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const HeroHome = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-20 bg-home-mobile bg-cover bg-fixed bg-center bg-no-repeat px-hero pt-hero-home-pt pb-hero-home-pb md:bg-home-tablet lg:flex-row lg:items-end lg:justify-between lg:bg-home-desktop">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 bg-home-mobile bg-cover bg-fixed bg-center bg-no-repeat px-hero pt-28 pb-hero-home-pb md:bg-home-tablet md:pt-52 lg:flex-row lg:items-end lg:justify-between lg:bg-home-desktop xl:pt-96">
       <HeroText />
       <Button />
     </main>

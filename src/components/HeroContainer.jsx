ead816e9031508a02fce96249f3019f7d431c0fa
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-08 20:58:47 +0800

    modify padding top on tablet

--- src/components/HeroContainer.jsx
+++ src/components/HeroContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroContainer = (props) => {
   return (
     <main
-      className={`${props.px} ${props.pb} ${props.bg} flex min-h-screen flex-col gap-8 bg-cover bg-fixed bg-center bg-no-repeat pt-20`}
+      className={`${props.px} ${props.pb} ${props.bg} flex min-h-screen flex-col gap-8 bg-cover bg-fixed bg-center bg-no-repeat pt-20 md:pt-[8.5rem]`}
     >
       {props.children}
     </main>

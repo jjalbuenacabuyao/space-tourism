f5127f944ccd6c41c813a8c88b684e14e694a329
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-14 18:35:18 +0800

    added opacity

--- src/components/NumberedHeading.jsx
+++ src/components/NumberedHeading.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const NumberedHeading = (props) => {
   return (
-    <h1 className="text-center font-barlow-condensed text-base uppercase tracking-[2.7px]">
-      <span className="mr-4 font-bold" aria-hidden="true">
+    <h1 className="text-center font-barlow-condensed text-base uppercase tracking-[2.7px] md:text-left md:text-xl lg:text-[1.75rem]">
+      <span className="mr-4 font-bold text-white opacity-25" aria-hidden="true">
         {props.num}
       </span>
       {props.text}

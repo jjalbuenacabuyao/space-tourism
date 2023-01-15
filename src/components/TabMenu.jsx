fc74015c491358228339dcb985ee5597db428ff1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-15 12:29:10 +0800

    added order

--- src/components/TabMenu.jsx
+++ src/components/TabMenu.jsx
@@ -21,22 +21,23 @@ const TabMenu = () => {
     <div className="flex flex-col items-center gap-8 text-center px-hero lg:grid lg:grid-cols-2 lg:items-center">
       <Image
         border="false"
+        order="order-1"
         webpSrc={active.images.webp}
         pngSrc={active.images.png}
         alt={active.imgAlt}
         className="aspect-square w-img-responsive m-auto"
       />
 
-      <article className="flex flex-col gap-5 lg:gap-8 lg:text-left">
-        <ul className="flex items-center gap-6 text-sm justify-center lg:justify-start lg:text-base lg:gap-8">{listItems}</ul>
-        <TextContainer paddingBottom="pb-8" bordered="true">
+      <article className="flex flex-col gap-5 lg:gap-8 lg:text-left order-2">
+        <ul className="flex items-center gap-6 text-sm justify-center lg:justify-start lg:text-base lg:gap-8 order-1">{listItems}</ul>
+        <TextContainer order="order-2" paddingBottom="pb-8" bordered="true">
           <h1 className="font-bellefair text-planet-name uppercase leading-[1.2em]">
             {active.name}
           </h1>
           <p>{active.description}</p>
         </TextContainer>
 
-        <div className="flex flex-col gap-8 pt-8 lg:flex-row lg:gap-20">
+        <div className="flex flex-col gap-8 pt-8 lg:flex-row lg:gap-20 order-3">
           <DistanceTravel
             title={active.distanceTitle}
             value={active.distance}

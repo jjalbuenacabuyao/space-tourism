d6e6306cae253be3ea62601f2df73f179590bb0f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-15 13:36:19 +0800

    added props.className

--- src/components/NumberedHeading.jsx
+++ src/components/NumberedHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberedHeading = (props) => {
   return (
-    <h1 className="text-center font-barlow-condensed text-base uppercase tracking-[2.7px] md:pl-10 md:text-left md:text-xl lg:px-hero lg:text-[1.75rem]">
+    <h1 className={`text-center font-barlow-condensed text-base uppercase tracking-[2.7px] md:text-left md:text-xl lg:text-[1.75rem] ${props.className}`}>
       <span className="mr-4 font-bold text-white opacity-25" aria-hidden="true">
         {props.num}
       </span>

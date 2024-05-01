595b0c0f93f60b62407aeea74a2bde53366ab8f7
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-01 20:24:04 +0800

    adjust font-sizes and spacing for md screens

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,16 +3,16 @@ import { Header } from "../components";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-home-mobile bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-home-mobile bg-cover bg-no-repeat md:bg-home-tablet">
       <Header />
-      <main className="flex flex-col items-center gap-20 p-6">
+      <main className="flex flex-col items-center gap-20 p-6 md:px-40 md:pt-28 md:gap-40 md:pb-24">
         <div className="flex flex-col gap-10 text-center">
           <h1 className="uppercase">
-            <span className="font-barlow-condensed tracking-[2.7px]">
+            <span className="font-barlow-condensed tracking-[2.7px] md:text-xl md:tracking-[3.38px]">
               So, you want to travel to
             </span>
             <br />
-            <span className="font-bellefair text-7xl">Space</span>
+            <span className="font-bellefair text-7xl md:text-9xl">Space</span>
           </h1>
           <p>
             Let's face it; if you want to go to space, you might as well
@@ -22,7 +22,7 @@ const Home = () => {
           </p>
         </div>
 
-        <button className="rounded-full bg-white py-16 px-[30px] font-bellefair text-xl uppercase leading-none tracking-[1.25px] text-clr-dark">
+        <button className="rounded-full bg-white py-16 px-[30px] font-bellefair text-xl uppercase leading-none tracking-[1.25px] text-clr-dark md:px-16 md:py-28 md:text-3xl md:tracking-[2px]">
           Explore
         </button>
       </main>

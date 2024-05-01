2e7da9208afb3c165c90f05bc331799112a80ffd
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-01 20:56:46 +0800

    adjust spacing and layout on lg screens

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,10 +3,10 @@ import { Header } from "../components";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-home-mobile bg-cover bg-no-repeat md:bg-home-tablet">
+    <div className="min-h-screen bg-home-mobile bg-cover bg-no-repeat md:bg-home-tablet lg:bg-home-desktop">
       <Header />
-      <main className="flex flex-col items-center gap-20 p-6 md:px-40 md:pt-28 md:gap-40 md:pb-24">
-        <div className="flex flex-col gap-10 text-center">
+      <main className="flex flex-col items-center gap-20 p-6 md:px-40 md:pt-28 md:gap-40 md:pb-24 lg:grid lg:grid-cols-2 lg:px-28">
+        <div className="flex flex-col gap-10 text-center lg:text-left">
           <h1 className="uppercase">
             <span className="font-barlow-condensed tracking-[2.7px] md:text-xl md:tracking-[3.38px]">
               So, you want to travel to
@@ -14,7 +14,7 @@ const Home = () => {
             <br />
             <span className="font-bellefair text-7xl md:text-9xl">Space</span>
           </h1>
-          <p>
+          <p className="max-w-md">
             Let's face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we'll give you a truly out of this
@@ -22,7 +22,7 @@ const Home = () => {
           </p>
         </div>
 
-        <button className="rounded-full bg-white py-16 px-[30px] font-bellefair text-xl uppercase leading-none tracking-[1.25px] text-clr-dark md:px-16 md:py-28 md:text-3xl md:tracking-[2px]">
+        <button className="rounded-full bg-white py-16 px-[30px] font-bellefair text-xl uppercase leading-none tracking-[1.25px] text-clr-dark md:px-16 md:py-28 md:text-3xl md:tracking-[2px] self-end justify-self-end">
           Explore
         </button>
       </main>

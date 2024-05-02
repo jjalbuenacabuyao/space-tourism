ac0ab3bd64feb40b427cb1e64e1f1d5042ab2188
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-02 22:27:38 +0800

    adjust left padding on lg screens

--- src/components/NumberedHeading.jsx
+++ src/components/NumberedHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberedHeading = ({ index, title }) => {
   return (
-    <h1 className="flex justify-center gap-4 pb-8 font-barlow-condensed tracking-[2.7px] md:justify-start md:pl-10 md:pt-10 md:pb-16 md:text-xl">
+    <h1 className="flex justify-center gap-4 pb-8 font-barlow-condensed tracking-[2.7px] md:justify-start md:pl-10 md:pt-10 md:pb-16 md:text-xl lg:pl-16">
       <span className="font-bold text-white/25">0{index}</span>
       <span className="uppercase">{title}</span>
     </h1>

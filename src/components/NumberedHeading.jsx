e9729096c815e9238c09ce974eee59a7fca623ea
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-02 22:04:29 +0800

    add padding in md screens

--- src/components/NumberedHeading.jsx
+++ src/components/NumberedHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberedHeading = ({ index, title }) => {
   return (
-    <h1 className="flex justify-center gap-4 font-barlow-condensed tracking-[2.7px]">
+    <h1 className="flex justify-center gap-4 font-barlow-condensed tracking-[2.7px] pb-8 md:justify-start md:pl-10 md:pt-10">
       <span className="font-bold text-white/25">0{index}</span>
       <span className="uppercase">{title}</span>
     </h1>

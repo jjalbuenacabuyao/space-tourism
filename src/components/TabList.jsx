863f4f876e88fbf5df2d52bb089ec6b46a1bec51
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-04 20:32:22 +0800

    add style for circle button

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -1,19 +1,28 @@
 import { List, Trigger } from "@radix-ui/react-tabs";
 
-const TabList = ({triggers, type, label}) => {
+const TabList = ({ triggers, type, label }) => {
   return (
-    <List aria-label={label} className="flex gap-6">
+    <List
+      aria-label={label}
+      className={`flex ${type === "circles" ? "gap-4" : "gap-6"}`}
+    >
       {triggers.map((trigger) => (
         <Trigger
           key={trigger}
           value={trigger}
-          className="box-border border-b-[3px] border-b-transparent pb-2 font-barlow-condensed text-sm uppercase tracking-[2.36px] text-off-white aria-selected:border-b-white aria-selected:text-white md:text-base"
+          className={`${
+            type === "circles"
+              ? "bg-off-white w-[0.625rem] aspect-square aria-selected:bg-white rounded-full"
+              : "box-border border-b-[3px] border-b-transparent pb-2 font-barlow-condensed text-sm uppercase tracking-[2.36px] text-off-white aria-selected:border-b-white aria-selected:text-white md:text-base"
+          }`}
         >
-          {trigger}
+          <span className={`${type === "circles" ? "hidden" : ""}`}>
+            {trigger}
+          </span>
         </Trigger>
       ))}
     </List>
   );
-}
+};
 
-export default TabList
+export default TabList;

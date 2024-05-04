de7187e00f5604e684e67383497d28e3d35ac69a
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-04 20:08:12 +0800

    export TabImageContent, TabList, TabRoot, and TabTextContent

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
 import Header from "./Header";
 import NumberedHeading from "./NumberedHeading";
+import TabImageContent from "./TabImageContent";
+import TabList from "./TabList";
+import TabRoot from "./TabRoot";
+import TabTextContent from "./TabTextContent";
 
 export {
   Header,
-  NumberedHeading
+  NumberedHeading,
+  TabImageContent,
+  TabList,
+  TabRoot,
+  TabTextContent
 }

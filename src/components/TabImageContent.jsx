0950e7e44dfd8b6490ff4c63e9e445fa8c95811e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-05-04 20:08:52 +0800

    add border in crew

---
+++ src/components/TabImageContent.jsx
@@ -0,0 +1,22 @@
+import { Content } from "@radix-ui/react-tabs";
+
+const TabImageContent = ({ title, img, page }) => {
+  return (
+    <Content
+      key={title}
+      value={title}
+      forceMount
+      className={`hidden data-[state=active]:block lg:row-span-2 ${page === "crew" ? "border-b border-b-off-white/25" : ""}`}
+    >
+      <img
+        src={img}
+        alt={`Photo of ${title}`}
+        className={`mx-auto ${
+          page === "destination" && "aspect-square w-44 md:w-80 xl:w-[28rem]"
+        } `}
+      />
+    </Content>
+  );
+};
+
+export default TabImageContent;
